e65a5da84f7470f55639101c97d19a266c4e6479
Juan Lopez <sebastian77790@gmail.com>
2017-07-05 20:09:41 -0500

    inmutable function

--- functions.js
+++ functions.js
@@ -15,37 +15,37 @@ var balance2 = 0;
 
 function imprimirBalance(balance){
 	// Imprimir balance
-	console.info("Tienes: $" + window.balance);
+	console.info("Tienes: $" + balance);
 }
 
 function retirarDinero(dinero){
 	// Solo puede retirar dinero si es menor que el balance
 	if(balance > 0){
-		window.balance -= dinero;
-		console.log("Has retirado: $" + dinero + ", tu balance es: $" + window.balance);
+		balance -= dinero;
+		console.log("Has retirado: $" + dinero + ", tu balance es: $" + balance);
 	}
 }
 
 function transferir(balance, dinero, balance2) { 
 	// transferir balance a otro balance2   
-    if(window.balance > 0){
-		window.balance2 = window.balance2 + dinero;
-		window.balance -= dinero;
+    if(balance > 0){
+		balance2 = balance2 + dinero;
+		balance -= dinero;
 		console.log("Valor transferido: $" + dinero);
-		console.log("Tu balance actual es: $" + window.balance);
+		console.log("Tu balance actual es: $" + balance);
 	}
 }
 
 function imprimirBalance2(balance2) {
 	// Imprimir el balance2
-	console.info("El valor de balance2 es: $" + window.balance2);
+	console.info("El valor de balance2 es: $" + balance2);
 }
 
 function retirarTodoMiDinero(balance) {
 		// Retirar de 100 en 100
-		while(window.balance > 0){
-			window.balance -= 100;
-			console.log("Has retirado: $100, tu balance es:$" + window.balance);
+		while(balance > 0){
+			balance -= 100;
+			console.log("Has retirado: $100, tu balance es:$" + balance);
 		}	
  }
 
